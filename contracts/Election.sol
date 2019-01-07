5d18d9d3bb25bb0fed24c06395efbc557481f60d
rosbell <ros.bell@manchester.ac.uk>
2019-01-07 15:26:33 +0000

    intial commit - smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+  //constructor - set the value of a variable
+string public candidate;
+
+constructor () public {
+  candidate = "Candidate 1"; //state variable, data belongs to the whole contract
+
+}
+
+}
